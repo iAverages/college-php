836ce0a855249b079c0d9fe33f7c83c0ec69f3dc
iAverages <me@danielraybone.com>
2022-12-07 13:52:59 +0000

    basic sqlite

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
-
-echo "cancer";
+$dir = 'sqlite:./db.sqlite';
+$dbh  = new PDO($dir) or die("cannot open the database");
+$query =  "SELECT * FROM Album";
+foreach ($dbh->query($query) as $row)
+{
+    echo $row[0] . "<br />";
+}
+$dbh = null; //This is how you close a PDO connection
